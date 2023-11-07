5bad248c0801aad6a201fa7c72e7a8fc18056dbe
ErasKuKU <149342667+ErasKuKU@users.noreply.github.com>
2023-11-07 00:10:59 +0000

    Add files via upload

--- App.java
+++ App.java
@@ -1,56 +1,45 @@
 public class App {
     public static void main(String[] args) throws Exception {
-       person student1=new person("kofi", 2000);
-       student student2=new student("Yaw", 2005, "computer science");
-        instructor student3=new instructor("adloa", 2007, 7000);
+       
+        employee work = new employee();
+        manager manager1 = new manager();
+        executive excutive1 = new executive();
 
-        System.out.println("The salary of the instructor is " + student3.getsalary());
-        System.out.println("The name of the student is " + student2.getname());
+        work.name();
+        manager1.name();
+        manager1.salary();
+        manager1.department();
+        excutive1.name();
 
     }
 
 
 }
-
-class person{
+class employee{
     String name;
-    int yearofBirth;
-
-    public person(String name, int yearofBirth){
-        this.name=name;
-        this.yearofBirth=yearofBirth;
-    }
-        public String getname(){
-        return name;
-    }
-    public int getyearofBirth(){
-        return yearofBirth;
-    }
-
-    }
-
-
-  class student extends person{
-    String major;
-public student(String name, int yearofBirth, String major){
-    super(name, yearofBirth);
-    this.major=major;
-}
-
-    public String getmajor(){
-        return major;
-    }
-}
-
-  class instructor extends person{
     int salary;
-    public instructor(String name, int yearofBirth, int salary){
-    super(name, yearofBirth);
-    this.salary=salary;
 
+   public void name(){
+        System.out.println(" Wadon");
     }
-    public int getsalary(){
-        return salary;
+    public void salary(){
+        System.out.println(7000);
     }
+    
 
-  }
+   
+}
+class manager extends employee{
+    String department;
+
+    public void department(){
+        System.out.println("Computer Science");
+    }
+    
+
+}
+
+class executive extends manager{
+
+}
+  
